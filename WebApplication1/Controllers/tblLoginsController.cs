3ac005d24f520439274369524a5cab36b2263b59
mitschi11051994 <mitschi1994@hotmail.com>
2017-07-09 17:45:04 -0600

    ADD CONTROLLER

---
+++ WebApplication1/Controllers/tblLoginsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class tblLoginsController : Controller
+    {
+        private CMDEntities db = new CMDEntities();
+
+        // GET: tblLogins
+        public ActionResult Index()
+        {
+            return View(db.tblLogin.ToList());
+        }
+
+        // GET: tblLogins/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblLogin tblLogin = db.tblLogin.Find(id);
+            if (tblLogin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblLogin);
+        }
+
+        // GET: tblLogins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblLogins/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Username,Password,Role")] tblLogin tblLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblLogin.Add(tblLogin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblLogin);
+        }
+
+        // GET: tblLogins/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblLogin tblLogin = db.tblLogin.Find(id);
+            if (tblLogin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblLogin);
+        }
+
+        // POST: tblLogins/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Username,Password,Role")] tblLogin tblLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblLogin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblLogin);
+        }
+
+        // GET: tblLogins/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblLogin tblLogin = db.tblLogin.Find(id);
+            if (tblLogin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblLogin);
+        }
+
+        // POST: tblLogins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            tblLogin tblLogin = db.tblLogin.Find(id);
+            db.tblLogin.Remove(tblLogin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
